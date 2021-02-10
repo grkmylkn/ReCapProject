68a5e73c1fdf5e61c5bc3be68d6e0b38a4ad3da9
Görkem <gorkemyelken@gmail.com>
2021-02-10 16:16:46 +0300

    Add project files.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,12 +8,6 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-
-            foreach (var car in carManager.GetAll())
-            {
-                Console.WriteLine(car.Id);
-            }
         }
     }
 }
